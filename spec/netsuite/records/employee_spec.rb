a207ede2937b80a844409c000cd7f0b7cf673ecd
Eric Collins <eric@thoughtbot.com>
2015-03-03 18:10:56 -0500

    Add more to the employee

--- spec/netsuite/records/employee_spec.rb
+++ spec/netsuite/records/employee_spec.rb
@@ -9,9 +9,9 @@ describe NetSuite::Records::Employee do
       :comments, :date_created, :direct_deposit, :eligible_for_commission,
       :email, :employee_status, :employee_type, :entity_id, :expense_limit,
       :fax, :first_name, :hire_date, :home_phone, :is_inactive, :is_job_resource,
-      :job_description, :labor_cost, :last_modified_date, :last_name, :middle_name,
-      :mobile_phone, :next_review_date, :office_phone, :pay_frequency, :phone,
-      :phonetic_name, :purchase_order_approval_limit, :purchase_order_approver,
+      :job_description, :klass, :labor_cost, :last_modified_date, :last_name,
+      :middle_name, :mobile_phone, :next_review_date, :office_phone, :pay_frequency,
+      :phone, :phonetic_name, :purchase_order_approval_limit, :purchase_order_approver,
       :purchase_order_limit, :release_date, :resident_status, :salutation,
       :social_security_number, :title, :visa_exp_date, :visa_type
     ].each do |field|
